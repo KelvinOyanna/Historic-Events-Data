9ea4201c425a1631a3fabdf327a99795caedc6f2
KelvinOyanna <dotkelplus@gmail.com>
2021-07-31 22:53:37 +0100

    add event function

--- eventsScrapper.py
+++ eventsScrapper.py
@@ -7,5 +7,25 @@ historic event data
 """
 
 def generate_url(month, day):
-    url = 'https://www.onthisday.com/day/{month}/{day}'
+    url = f"https://www.onthisday.com/day/{month}/{day}"
     return url
+
+def get_page(url):
+    page = requests.get(url)
+    soup = BeautifulSoup(page.content, "html.parser")
+    return soup
+
+def events_of_the_day(month, day):
+    """
+    Return the events of a given day
+    """
+    url = generate_url(month, day)
+    page = get_page(url)
+    raw_events = page.find_all(class_ = "event")
+    events = [event.text for event in raw_events]
+    print(events[0])
+
+
+events_of_the_day("july", 10)
+# get_page("https://www.onthisday.com/day/july/10")
+# print(generate_url('july', 5))
