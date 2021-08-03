bd152163864c7e7cb48a85863c05efde02f8d9fe
KelvinOyanna <dotkelplus@gmail.com>
2021-08-03 12:13:47 +0100

    Added a module to generate event and export scrapepd data to a csv file

--- eventsScrapper.py
+++ eventsScrapper.py
@@ -1,21 +1,23 @@
 import requests
+from typing import List, Iterator
 from bs4 import BeautifulSoup
+import pandas as pd
 
 """
 This module is a web scrapper that sends a request to https://www.onthisday.com/ and pulls
 historic event data
 """
 
-def generate_url(month, day):
+def generate_url(month: str, day: str) -> str:
     url = f"https://www.onthisday.com/day/{month}/{day}"
     return url
 
-def get_page(url):
+def get_page(url: str) -> Iterator[str]:
     page = requests.get(url)
     soup = BeautifulSoup(page.content, "html.parser")
     return soup
 
-def events_of_the_day(month, day):
+def events_of_the_day(month: str, day: int) -> List[str]:
     """
     Return the events of a given day
     """
@@ -23,4 +25,5 @@ def events_of_the_day(month, day):
     page = get_page(url)
     raw_events = page.find_all(class_ = "event")
     events = [event.text for event in raw_events]
-    return events
+    event_rows = [row for row in events]
+    return event_rows
---
+++ exportData.py
@@ -0,0 +1,28 @@
+from typing import Iterator
+import datetime as dt
+import eventsScrapper as scrapper
+import pandas as pd 
+import os
+
+
+def date_range(start_date: dt.date, end_date: dt.date) -> Iterator[dt.date]:
+    for n in range(int((end_date - start_date).days)):
+        yield start_date + dt.timedelta(n)
+
+def create_events():
+    events_df = pd.DataFrame()
+    start_date = dt.date(2020, 1, 1)
+    end_date = dt.date(2020, 1, 10 )
+    for date in date_range(start_date, end_date):
+        month = date.strftime("%B").lower()
+        event_rows = scrapper.events_of_the_day(month, date.day)
+        month_df = pd.DataFrame(event_rows, columns=['Events'])
+        events_df = events_df.append(month_df)
+    return events_df
+
+def export_data():
+    if not os.path.exists(os.path.abspath('./') + '\RawEventsData'):
+        os.mkdir('RawEventsData')
+    events_df = create_events()
+    events_df.to_csv('RawEventsData/events.csv', index= False)
+
