db0e2a6ee19c8cb183d05b71a8e99dc4cbfa2e70
KelvinOyanna <dotkelplus@gmail.com>
2021-08-04 01:01:33 +0100

    refactored data transformation function

--- loadToDatabase.py
+++ loadToDatabase.py
@@ -2,7 +2,7 @@ from pandas.core.frame import DataFrame
 from exportData import export_data
 import pandas as pd
 import psycopg2
-import datetime as dt
+from datetime import datetime as dt
 from typing import Dict
 from dotenv import dotenv_values
 
@@ -13,23 +13,26 @@ def credentials() -> Dict:
 
 def transformData() -> DataFrame:
     # A helper function to extract the day and month in history an event occoured
-    def extractMonthDay(date_coln: str) ->str:
-        date_coln = dt.date(date_coln).strftime('%d %B')
-        return date_coln
+    def extractMonthDay(date_coln):
+        month_day = dt.strptime(str(date_coln), '%Y-%m-%d').date().strftime('%d %B')
+        return month_day
     # A helper function to extract the year an event occured
     def extractYearEventOccoured(event):
         year = event.split(' ')[0]
-        return year
+        if len(year) < 4:
+            return year + 'BC'
+        else:
+            return year
     # A helper function to extract only the event information
     def extractEvent(event):
         event = ' '.join(event.split(' ')[1:])
         return event
     try:
-        events_data = pd.read_csv('/RawEventsData/events.csv') #  Read event data from event file
-        events_data['Date In History'] = events_data['Date'].apply(extractMonthDay)
+        events_data = pd.read_csv('RawEventsData/events.csv') #  Read event data from event file
+        events_data['This Day'] = events_data['Date'].apply(extractMonthDay)
         events_data['Year Occured'] = events_data['Event'].apply(extractYearEventOccoured)
         events_data['Event'] = events_data['Event'].apply(extractEvent)
-        return events_data[['Date In History', 'Year Occured', 'Event']]
+        return events_data[['This Day', 'Year Occured', 'Event']]
     except FileNotFoundError:
         print('Kindly check that events.csv & its folder exist and try again')
     
@@ -37,6 +40,5 @@ def transformData() -> DataFrame:
 
 
 #def loadToPostgres():
-
 df = transformData()
-#print(df.head())
+print(df.head(10))
